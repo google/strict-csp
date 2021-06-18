e82c5fb21f4ef445020dfb3057d6a49c9c826c2a
maudnals <maud.nalpas@gmail.com>
2021-06-18 11:45:59 +0200

    Updated the setup and undo setup scripts to remove the React app

--- undo-dev-setup.sh
+++ undo-dev-setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 cd strict-csp-html-webpack-plugin && npm unlink 'strict-csp' && cd ..
-cd react-app && npm unlink 'strict-csp-html-webpack-plugin' && cd ..
 cd strict-csp && rm -rf node_modules && cd ..
-cd react-app && rm -rf node_modules && cd ..
 cd strict-csp-html-webpack-plugin && rm -rf node_modules && cd ..
